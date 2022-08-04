6b2cd97335326fb176feac079bddbcaa8408406d
Mauricio Salerno <msalerno@pa.uc3m.es>
2022-08-04 11:13:29 +0200

    Remove ae experiments script

--- experiments/action-elimination/ae_parser.py
+++
@@ -1,7 +0,0 @@
-from lab.parser import Parser
-
-# Parse AE specific values
-ae_parser = Parser()
-ae_parser.add_pattern("old_cost", r"Old plan cost: (\d+)", type=int)
-ae_parser.add_pattern("new_cost", r"New plan cost: (\d+)", type=int)
-ae_parser.parse()
